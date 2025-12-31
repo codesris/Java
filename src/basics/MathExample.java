274aa878d4118cc079ae7c057481be0ceafc5119
Srisan Chaudhary <srisanchaudhary07@gmail.com>
2025-12-31 15:33:25 +0545

    Math Example

---
+++ src/basics/MathExample.java
@@ -0,0 +1,28 @@
+package src.basics;
+
+public class MathExample {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double power = Math.pow(3,4);
+        double abs = Math.abs(-5);
+        double sqrt = Math.sqrt(9);
+        long round = Math.round(3.14);
+        double ceil = Math.ceil(3.14);
+        double floor = Math.floor(3.14);
+        int max = Math.max(3,14);
+        int min = Math.min(3,14);
+
+        System.out.println(power);
+        System.out.println(abs);
+        System.out.println(sqrt);
+        System.out.println(round);
+        System.out.println(ceil);
+        System.out.println(floor);
+        System.out.println(max);
+        System.out.println(min);
+
+
+    }   
+}
