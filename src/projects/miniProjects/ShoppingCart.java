71b29ffe1531d9a35b56a41136f55904f9bcf057
Srisan Chaudhary <srisanchaudhary07@gmail.com>
2025-12-31 09:12:14 +0545

    Operators

---
+++ src/projects/miniProjects/ShoppingCart.java
@@ -0,0 +1,30 @@
+package src.projects.miniProjects;
+
+import java.util.Scanner;
+
+public class ShoppingCart {
+    public static void main (String[] args){
+        String item;
+        double price;
+        int num;
+        char currency = '$';
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
+
+        System.out.print("\nWhat is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("\nHow many would you like to buy?: ");
+        num = scanner.nextInt();
+
+        System.out.println("You have bought " + num + item);
+        System.out.println("Your total is : " + currency + price * num);
+
+        scanner.close();
+    }
+}
