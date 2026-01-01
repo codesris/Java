5f1ec2a14dbfb5f413c21e21d9a3fae7802b316d
Srisan Chaudhary <srisanchaudhary07@gmail.com>
2026-01-01 21:27:44 +0545

    For loop

---
+++ src/basics/ForLoops.java
@@ -0,0 +1,12 @@
+package src.basics;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        //for loop executes a code for certain amount of time
+        int i;
+        for(i=10;i>=0;i--){
+            System.out.println(i);
+        }
+        System.out.println("Happy New Year!!!");
+    }
+}
