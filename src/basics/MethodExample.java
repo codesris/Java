802b0d3524464f0789435f9888cda031990ff9fb
Srisan Chaudhary <srisanchaudhary07@gmail.com>
2026-01-03 21:05:13 +0545

    MethodExample

---
+++ src/basics/MethodExample.java
@@ -0,0 +1,62 @@
+package src.basics;
+
+public class MethodExample {
+ public static void main(String[] args) {
+
+    //method()  a block of reusable code that is executed whenever it is called.
+
+    String name = "Chasris";
+    int age = 22;
+    happyBirthday(name, age);
+    System.out.println();
+
+    double result = squareMethod(3);
+    System.out.println(result);
+
+    System.out.println(cubeMethod(3));
+    System.out.println();
+
+    String fullName = getFullName("Spongebob", "SquarePants");
+    System.out.println(fullName);
+    System.out.println();
+
+    int old = 21;
+    if (ageCheck(old)){
+        System.out.println("You may sign up");
+    }
+    else{
+        System.out.println("You need to be 18+ to sign up");
+    }
+ }  
+ //happyBirthday() method
+static void happyBirthday(String name,int age){
+    System.out.println("Happy Birthday "+name);
+    System.out.println("You are now "+ age+ " years old");
+ }
+
+ //squareMethod()
+ static double squareMethod (double number){
+
+    return number * number;
+ }
+
+ //cubeMethod()
+ static double cubeMethod (double number){
+    return Math.pow(number, 3);
+ }
+
+ //getFullName() method
+ static String getFullName(String first, String last){
+    return first + " " + last;
+ }
+
+ static boolean ageCheck (int old){
+    return old >= 18;
+    // same as: if (age>=18){
+    //          return true;
+    //          }
+    //          else{
+    //          return false;
+    //          };
+ }
+}
